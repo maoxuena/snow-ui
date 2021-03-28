5967fd064f1e125122cb353ea235526365497734
maoxuena <maoxuena@foxmail.com>
2021-03-28 18:51:35 +0800

    增加 icon 和 iconPosition 属性

--- src/button.vue
+++ src/button.vue
@@ -1,9 +1,28 @@
 <template>
-  <button class="s-button">按钮</button>
+  <button class="s-button"
+          :class="{[`icon-${iconPosition}`]:true}">
+    <svg v-if="icon"
+         class="icon">
+      <use :xlink:href="`#icon-${icon}`"></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 
 <script>
 export default {
+  props: {
+    icon: {
+      type: String,
+      default: ''
+    },
+    iconPosition: {
+      type: String,
+      default: 'default'
+    }
+  }
 
 }
 </script>
@@ -17,6 +36,10 @@ export default {
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background-color: var(--button-bg);
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
 
   &:hover {
     border-color: var(--border-color-hover);
@@ -29,6 +52,27 @@ export default {
 
   &:focus {
     outline: none;
+  }
+
+  > .icon {
+    order: 1;
+    margin-right: 0.2em;
+  }
+
+  > .content {
+    order: 2;
+  }
+
+  &.icon-right {
+    > .icon {
+      order: 2;
+      margin-left: 0.2em;
+      margin-right: 0;
+    }
+
+    > .content {
+      order: 1;
+    }
   }
 }
 </style>
