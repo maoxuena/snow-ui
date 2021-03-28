c91418a556d3fcfc4aa02b32c49f954ae11fb27a
maoxuena <maoxuena@foxmail.com>
2021-03-28 20:03:58 +0800

    创建 button group

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 import Vue from 'vue'
 import Button from './button'
+import ButtonGroup from './button-group'
 import Icon from './icon'
 
 Vue.component('s-button', Button)
+Vue.component('s-button-group', ButtonGroup)
 Vue.component('s-icon', Icon)
 
 new Vue({
   el: '#app',
   data: {
-    loading:false
+    loading: false
   }
 })
