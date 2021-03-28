d94a13025059e8cd282506a847286373170a3c48
maoxuena <maoxuena@foxmail.com>
2021-03-28 21:30:02 +0800

    使用 chai.expect 添加四个测试用例

--- src/app.js
+++ src/app.js
@@ -13,3 +13,86 @@ new Vue({
     loading: false
   }
 })
+
+/**
+ * 单元测试
+ */
+import chai from 'chai'
+const expect = chai.expect
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  })
+  vm.$mount()
+  const useElement = vm.$el.querySelector('use')
+  const href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#icon-settings')
+  vm.$el.remove()
+  vm.$destroy()
+}
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+      loading: true
+    }
+  })
+  vm.$mount()
+  const useElement = vm.$el.querySelector('use')
+  const href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#icon-loading')
+  vm.$el.remove()
+  vm.$destroy()
+}
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  })
+  vm.$mount(div)
+  const svgElement = vm.$el.querySelector('svg')
+  const { order } = window.getComputedStyle(svgElement)
+  expect(order).to.eq('1')
+  vm.$el.remove()
+  vm.$destroy()
+}
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+      iconPosition: 'right'
+    }
+  })
+  vm.$mount(div)
+  const svgElement = vm.$el.querySelector('svg')
+  const { order } = window.getComputedStyle(svgElement)
+  expect(order).to.eq('2')
+  vm.$el.remove()
+  vm.$destroy()
+}
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+    }
+  })
+  vm.$mount()
+  vm.$on('click', function () {
+    console.log(1)
+  })
+  // 希望这个函数被执行
+  let button = vm.$el
+  button.click()
+}
