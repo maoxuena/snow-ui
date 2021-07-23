0798cd4932d055847b485ed874d7eba09dcea7c1
maoxuena <maoxuena@foxmail.com>
2021-07-23 13:39:19 +0800

    col宽度设置

--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ import Button from './button'
 import ButtonGroup from './button-group'
 import Icon from './icon'
 import Input from './input'
+import Row from './Row'
+import Col from './Col'
 
 Vue.component('s-button', Button)
 Vue.component('s-button-group', ButtonGroup)
 Vue.component('s-icon', Icon)
 Vue.component('s-input', Input)
+Vue.component('s-row', Row)
+Vue.component('s-col', Col)
 
 new Vue({
   el: '#app',
---
+++ src/col.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="col" :class="[`col-${span}`]">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name:'SnowCol',
+  props: {
+    span: {
+      type: [Number, String]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.col{
+  $class-prefix: col-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n}{
+      width: ($n/24)*100%
+    }
+  }
+}
+</style>
