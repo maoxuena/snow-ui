7f8781e8eabba9d23fcecc653ce3bc25c0539dbe
maoxuena <maoxuena@foxmail.com>
2021-07-23 14:34:28 +0800

    基本实现 row 和 col

--- src/row.vue
+++ src/row.vue
@@ -1,8 +1,26 @@
 <template>
-  <div class="row">
+  <div class="row" :style="{marginLeft: -gutter/2+'px', marginRight: -gutter/2+'px'}">
     <slot></slot>
   </div>
 </template>
+<script>
+export default {
+  name:'SnowRow',
+  props: {
+    gutter: {
+      type: [Number, String]
+    }
+  },
+  created(){
+    
+  },
+  mounted(){
+    this.$children.forEach((vm) => {
+      vm.gutter = this.gutter
+    })
+  }
+}
+</script>
 <style lang="scss" scoped>
 .row{
   display: flex;
