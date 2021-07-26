6c04842c6b183155c6e534e31a3a329756f2d907
maoxuena <maoxuena@foxmail.com>
2021-07-26 17:05:00 +0800

    完成 tab 切换动画

--- src/tabs-head.vue
+++ src/tabs-head.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="tabs-head">
     <slot></slot>
+    <div class="line" ref="line"></div>
     <div class="actions-wrapper">
       <slot name="actions"></slot>
     </div>
@@ -9,16 +10,32 @@
 <script>
   export default {
     name: 'SnowTabsHead',
-    inject: ['eventBus']
+    inject: ['eventBus'],
+    created () {
+      this.eventBus.$on('update:selected', (item, vm) => {
+        let {width, height, top, left} = vm.$el.getBoundingClientRect()
+        this.$refs.line.style.width = `${width}px`
+        this.$refs.line.style.transform = `translateX(${left - 20}px)`
+        // this.$refs.line.style.left = `${left - 20}px`
+      })
+    }
   }
 </script>
 <style lang="scss" scoped>
 $tab-height: 40px;
+$blue: blue;
 .tabs-head {
   display: flex;
   height: $tab-height;
   justify-content: flex-start;
-  border: 1px solid red;
+  position: relative;
+
+  > .line {
+    position: absolute;
+    bottom: 0;
+    border-bottom: 1px solid $blue;
+    transition: all 350ms;
+  }
 
   > .actions-wrapper {
     margin-left: auto;
--- src/tabs.vue
+++ src/tabs.vue
@@ -31,7 +31,15 @@
       }
     },
     mounted(){
-      this.eventBus.$emit('update:selected', this.selected)
+      this.$children.forEach((vm) => {
+        if (vm.$options.name === 'SnowTabsHead') {
+          vm.$children.forEach((childVm) => {
+            if (childVm.$options.name === 'SnowTabsItem' && childVm.name === this.selected) {
+              this.eventBus.$emit('update:selected', this.selected, childVm)
+            }
+          })
+        }
+      })
     }
   }
 </script>
