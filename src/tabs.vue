190f969bb85d1a3f27054e85c96463c4ec187e3a
maoxuena <maoxuena@foxmail.com>
2021-07-28 12:22:25 +0800

    重构 tabs.vue

--- src/tabs.vue
+++ src/tabs.vue
@@ -30,19 +30,27 @@
         eventBus: this.eventBus
       }
     },
-    mounted(){
-      if(this.$children.length === 0){
-        console && console.warn && console.warn('tabs的子组件应该是 tabs-head 和 tabs-body，但你没有写')
-      }
-      this.$children.forEach((vm) => {
-        if (vm.$options.name === 'SnowTabsHead') {
-          vm.$children.forEach((childVm) => {
-            if (childVm.$options.name === 'SnowTabsItem' && childVm.name === this.selected) {
-              this.eventBus.$emit('update:selected', this.selected, childVm)
-            }
-          })
+    methods: {
+      checkChildren(){
+        if(this.$children.length === 0){
+          console && console.warn && console.warn('tabs的子组件应该是 tabs-head 和 tabs-body，但你没有写')
         }
-      })
+      },
+      selectTab(){
+        this.$children.forEach((vm) => {
+          if (vm.$options.name === 'SnowTabsHead') {
+            vm.$children.forEach((childVm) => {
+              if (childVm.$options.name === 'SnowTabsItem' && childVm.name === this.selected) {
+                this.eventBus.$emit('update:selected', this.selected, childVm)
+              }
+            })
+          }
+        })
+      }
+    },
+    mounted(){
+      this.checkChildren()
+      this.selectTab()
     }
   }
 </script>
